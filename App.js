f5e1d6d03ce3d1a5ea7eb2168afb7844870bb779
idsherma <idsherma@gmail.com>
2020-07-17 19:30:59 -0400

    styling text

--- App.js
+++ App.js
@@ -14,37 +14,22 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   console.log("App executed");
 
-  //padding + margin
+  //Styling Text
   return (
     <View style={{
       flex: 1, 
       justifyContent: 'center', 
       alignItems: 'center'
     }}>
-    <View style={{
-        backgroundColor: 'dodgerblue', 
-        width: 100, 
-        height: 100, 
-        padding: 20,
-        paddingHorizontal: 10, 
-        paddingLeft: 30
-      }}>
-        <View style={{
-          backgroundColor: 'gold', 
-          width: 50, 
-          height: 50
-        }}>
-        <View style={{
-          backgroundColor: 'tomato', 
-          width: 100, 
-          height: 100,
-          margin: 20
-        }}>
-      
-        </View>
-        </View>
-    </View>
-
+    <Text style={{
+     fontSize: 30, 
+     fontStyle: 'italic', 
+     fontWeight: "600", 
+     color: 'tomato', 
+     textTransform: 'capitalize', 
+     textAlign: 'center'
+     //fontFamily: "Roboto"
+    }}>I love react native</Text>
     </View>
   );
 }
