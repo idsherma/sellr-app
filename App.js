a0d984d020ad21e4f555ada66fd4ee47ea3a03b1
idsherma <idsherma@gmail.com>
2020-07-17 19:14:05 -0400

    shadows exercise

--- App.js
+++ App.js
@@ -25,9 +25,13 @@ export default function App() {
         backgroundColor: 'dodgerblue', 
         width: 100, 
         height: 100, 
-        borderWidth: 10, 
-        borderColor: 'royalblue', 
-        borderRadius: 10,
+        //iOS
+        shadowColor: 'grey', 
+        shadowOffset: {width: 0, height: 10}, 
+        shadowOpacity: 1,
+        shadowRadius: 10, 
+        //Android
+        elevation: 20
       }}>
     </View>
 
