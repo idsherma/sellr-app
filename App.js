89f4446817265543ab1b339b4cacdce2e47a7a9e
idsherma <idsherma@gmail.com>
2020-07-17 19:10:11 -0400

    borders

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import { StyleSheet, Text, View } from "react-native";
 
+
+// export default function App() {
+//   return (
+//     // <WelcomeScreen/>
+//    <ViewImageScreen/>
+//   );
+// }
 
 export default function App() {
+  console.log("App executed");
+
+  //borders
   return (
-    // <WelcomeScreen/>
-    <ViewImageScreen/>
+    <View style={{
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center'
+    }}>
+      <View style={{
+        backgroundColor: 'dodgerblue', 
+        width: 100, 
+        height: 100, 
+        borderWidth: 10, 
+        borderColor: 'royalblue', 
+        borderRadius: 10,
+      }}>
+    </View>
+
+    </View>
   );
 }
-
